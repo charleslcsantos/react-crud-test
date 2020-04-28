9b2b2d8960234d7a04e76f6a711fc534505be88c
Charles <charleslcsantos@gmail.com>
2020-04-28 20:44:42 -0300

    change nextjs to pure react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import User from "./components/user/User";
+
+function App() {
+  return <User></User>;
+}
+
+export default App;
--- stores/UserStore.ts
+++
@@ -1,7 +0,0 @@
-
-export interface IUser {
-}
-
-export const getAllUsers = async (): Promise<IUser[]> => {
-  return []
-};

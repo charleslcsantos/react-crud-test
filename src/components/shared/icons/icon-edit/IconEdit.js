e6ea7470dbb52df8aa4800194bfe3e4191dcd312
Charles <charleslcsantos@gmail.com>
2020-04-29 16:04:15 -0300

    added icon components

---
+++ src/components/shared/icons/icon-edit/IconEdit.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const IconEdit = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 18 18"
+    >
+      <g transform="translate(-728 -424.421)">
+        <g transform="translate(728 424.421)">
+          <path fill="#5d5d5d" d="M9,0A9,9,0,1,1,0,9,9,9,0,0,1,9,0Z" />
+          <path
+            fill="#fff"
+            d="M3,7.984V9.3H4.312l3.87-3.87L6.87,4.115ZM9.2,4.412a.348.348,0,0,0,0-.493L8.378,3.1a.348.348,0,0,0-.493,0l-.64.64L8.556,5.052l.64-.64Z"
+            transform="translate(2.85 2.852)"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+};
---
+++ src/components/shared/icons/icon-remove/IconRemove.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const IconRemove = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 18 18"
+    >
+      <g transform="translate(0 0.421)">
+        <circle
+          fill="#a96060"
+          cx="9"
+          cy="9"
+          r="9"
+          transform="translate(0 -0.421)"
+        />
+        <path
+          fill="#fff"
+          d="M5.444,10.105a.891.891,0,0,0,.888.888H9.885a.891.891,0,0,0,.888-.888V4.776H5.444Zm5.773-6.661H9.663L9.219,3H7l-.444.444H5v.888h6.217Z"
+          transform="translate(0.773 1.701)"
+        />
+      </g>
+    </svg>
+  );
+};
